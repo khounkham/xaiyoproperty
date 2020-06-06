ffcd20fb9f9ff128187042d0911db80e0b5a9ef4
khounkham <khounkham@gmail.com>
2020-06-06 23:11:05 +1200

    disable sending mail

--- contacts/views.py
+++ contacts/views.py
@@ -29,14 +29,14 @@ def contact(request):
     contact.save()
 
     # Send email
-
-    send_mail(
-        'Property Listing Inqury',
-        'There has been an inquiry for ' +listing+ '. Sign into the admin panel for more info',
-        'khounkham@gmail.com',
-        [realtor_email, 'khounkham@gmail.com'],
-        fail_silently=False
-    )
+# """ 
+#     send_mail(
+#         'Property Listing Inqury',
+#         'There has been an inquiry for ' +listing+ '. Sign into the admin panel for more info',
+#         'khounkham@gmail.com',
+#         [realtor_email, 'khounkham@gmail.com'],
+#         fail_silently=False
+#     ) """
 
     messages.success(request, 'Your request has been submitted, a realtor will get back to you soon')
 
